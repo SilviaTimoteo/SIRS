578ac292537c7b49e077b1c583f245f261fb5ba0
PatAFP <patricia_alexandrapereira@hotmail.com>
2015-12-02 15:04:53 +0000

    Exceptions

---
+++ DBServer/src/main/java/SIRS/ws/exceptions/InvalidTimestamp.java
@@ -0,0 +1,11 @@
+package SIRS.ws.exceptions;
+
+public class InvalidTimestamp extends RuntimeException {
+
+		   private static final long serialVersionUID = 1L;
+		   @Override
+		    public String getMessage() {
+				return "TIMEOUT: the timestamp sent is invalid\n";
+		    }
+
+}
---
+++ DBServer/src/main/java/SIRS/ws/exceptions/PatientDoesntExist.java
@@ -0,0 +1,11 @@
+package SIRS.ws.exceptions;
+
+public class PatientDoesntExist extends RuntimeException {
+
+		   private static final long serialVersionUID = 1L;
+		   @Override
+		    public String getMessage() {
+				return "The patient doesn't exist\n";
+		    }
+
+}
